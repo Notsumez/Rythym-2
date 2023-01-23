f1102dd934ecd02111d2478ef85b5505351166ea
notsumez <adahorapirola@gmail.com>
2023-01-23 17:20:17 -0300

    Artistas começo

--- artistas.php
+++ artistas.php
@@ -3,10 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/home.css">
+    <link rel="stylesheet" href="CSS/artistas.css">
+    <link rel="stylesheet" href="CSS/reset.css">
+    <link rel="stylesheet" href="CSS/all.min.css">
+    <link rel="shortcut icon" href="images/Rythym-pequeno-Logo.ico" type="image/x-icon">
     <title>Rythym - Artistas</title>
 </head>
 <body>
-    
+    <noscript>Você precisa ativar o JavaScript para rodar essa aplicação.</noscript>
+    <?php include('header.html');?>
 
+    <main class="pagina-artistas">
+        <div class="pagina-artistas-desktop">
+            <div class="tags-artistas">
+                <div class="tags-artistas-lista">
+                    <div class="tags-por-regiao">
+                        <div class="tags-por-regiao-titulo">
+                            <span>Nacionalidade</span>
+                        </div>
+                        <div class="tags-por-regiao-tipos">
+                            <a href="" class="item-regiao">Tudo</a>
+                            <a href="" class="item-regiao">Estados Unidos</a>
+                            <a href="" class="item-regiao">Brasil</a>
+                            <a href="" class="item-regiao">Inglaterra</a>
+                            <a href="" class="item-regiao">Indonesia</a>
+                            <a href="" class="item-regiao">India</a>
+                            <a href="" class="item-regiao">Germania</a>
+                            <a href="" class="item-regiao">Mexico</a>
+                            <a href="" class="item-regiao">Malasia</a>
+                            <a href="" class="item-regiao">Canada</a>
+                            <a href="" class="item-regiao">Sweden</a>
+                        </div>
+                    </div>
+                    <div class="tags-por-genero">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="info--lista-artistas">
+
+            </div>
+        </div>
+    </main>
 </body>
 </html>
